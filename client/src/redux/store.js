456bdbd797979aa8a84430fa769cd4eb73244813
YAKUT AHMEDIN <yakutmuder9@gmail.com>
2022-08-21 01:17:36 -0400

    dependency chack up

--- client/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import {productsReducer}  from "../redux/features/productSlices";
-
-export const store = configureStore({
-  reducer: {
-    getProduct: productsReducer
-  },
-});
